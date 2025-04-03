447a7512e0f52065867b889259828b9c1625ef26
rmadan0401 <rmadan@vecv.in>
2025-04-03 13:50:19 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,15 @@
-/*
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0") // ✅ Add this
+    }
 }
 
 plugins {
-    id("com.android.application")  // ✅ Keep only one
+    id("com.android.application")   // ✅ Correct placement
     id("com.github.triplet.play") version "3.12.1"
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
@@ -35,5 +22,3 @@ plugins {
 }
 
 apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
-
-
