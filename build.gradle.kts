0c91583df8b070843a270e56c5e0adcf010ce9cb
rmadan0401 <rmadan@vecv.in>
2025-04-07 11:57:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,28 @@
-// Root build.gradle.kts
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 plugins {
-    // Removed: alias(libs.plugins.android.application) ❌
-    id("com.github.triplet.play") version "3.12.1" // ✅ Custom plugin directly declared
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.spotless)
@@ -13,15 +33,4 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // No buildscript dependencies needed here right now
-    }
-}
-
-// If this file exists, keep it. It's likely configuring version catalog update rules.
 apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
